9670715b015d3660071456ab8bd721a0426dd0ab
mfs123 <45593802+mfs123@users.noreply.github.com>
2019-11-19 23:05:59 +0800

    大
    发生发大水

--- 后台系统/Backstage/src/router/index.js
+++ 后台系统/Backstage/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '../components/login/index.vue'
+import fogjet from '../components/login/forjet.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      component: login
+    }, {
+      path: '/forget',
+      component: fogjet
+    },
+	
   ]
 })
